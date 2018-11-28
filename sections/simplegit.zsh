93bc859ac047c7fe312d398347ba936fcc8fe8fd
Kalle Kietavainen <1026741+kkietava@users.noreply.github.com>
2018-11-28 10:05:15 +0200

    Add simplegit section

---
+++ sections/simplegit.zsh
@@ -0,0 +1,127 @@
+#
+# Git (simple version)
+#
+
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
+
+SPACESHIP_GIT_SHOW="${SPACESHIP_GIT_SHOW=true}"
+SPACESHIP_GIT_PREFIX="${SPACESHIP_GIT_PREFIX="on "}"
+SPACESHIP_GIT_SUFFIX="${SPACESHIP_GIT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_GIT_SYMBOL="${SPACESHIP_GIT_SYMBOL=" "}"
+
+# ------------------------------------------------------------------------------
+# Section (branch)
+# ------------------------------------------------------------------------------
+
+SPACESHIP_GIT_BRANCH_SHOW="${SPACESHIP_GIT_BRANCH_SHOW=true}"
+SPACESHIP_GIT_BRANCH_PREFIX="${SPACESHIP_GIT_BRANCH_PREFIX="$SPACESHIP_GIT_SYMBOL"}"
+SPACESHIP_GIT_BRANCH_SUFFIX="${SPACESHIP_GIT_BRANCH_SUFFIX=""}"
+SPACESHIP_GIT_BRANCH_COLOR="${SPACESHIP_GIT_BRANCH_COLOR="green"}"
+SPACESHIP_GIT_BRANCH_COLOR_DIRTY="${SPACESHIP_GIT_BRANCH_COLOR_DIRTY="yellow"}"
+
+spaceship_git_branch() {
+  [[ $SPACESHIP_GIT_BRANCH_SHOW == false ]] && return
+
+  local git_current_branch="$vcs_info_msg_0_"
+  [[ -z "$git_current_branch" ]] && return
+
+  git_current_branch="${git_current_branch#heads/}"
+  git_current_branch="${git_current_branch/.../}"
+
+  local branch_color=$SPACESHIP_GIT_BRANCH_COLOR
+  if [[ $(command git status -s --ignore-submodules=dirty 2> /dev/null) ]]; then
+      branch_color=$SPACESHIP_GIT_BRANCH_COLOR_DIRTY
+  fi
+
+
+  spaceship::section \
+    "$branch_color" \
+    "$SPACESHIP_GIT_BRANCH_PREFIX${git_current_branch}$SPACESHIP_GIT_BRANCH_SUFFIX"
+}
+
+# ------------------------------------------------------------------------------
+# Section (status)
+# ------------------------------------------------------------------------------
+
+SPACESHIP_GIT_STATUS_SHOW="${SPACESHIP_GIT_STATUS_SHOW=true}"
+SPACESHIP_GIT_STATUS_PREFIX="${SPACESHIP_GIT_STATUS_PREFIX=" ["}"
+SPACESHIP_GIT_STATUS_SUFFIX="${SPACESHIP_GIT_STATUS_SUFFIX="]"}"
+SPACESHIP_GIT_STATUS_COLOR="${SPACESHIP_GIT_STATUS_COLOR="red"}"
+SPACESHIP_GIT_STATUS_UNTRACKED="${SPACESHIP_GIT_STATUS_UNTRACKED="?"}"
+SPACESHIP_GIT_STATUS_ADDED="${SPACESHIP_GIT_STATUS_ADDED="+"}"
+SPACESHIP_GIT_STATUS_MODIFIED="${SPACESHIP_GIT_STATUS_MODIFIED="!"}"
+SPACESHIP_GIT_STATUS_RENAMED="${SPACESHIP_GIT_STATUS_RENAMED="»"}"
+SPACESHIP_GIT_STATUS_DELETED="${SPACESHIP_GIT_STATUS_DELETED="✘"}"
+SPACESHIP_GIT_STATUS_STASHED="${SPACESHIP_GIT_STATUS_STASHED="$"}"
+SPACESHIP_GIT_STATUS_UNMERGED="${SPACESHIP_GIT_STATUS_UNMERGED="="}"
+SPACESHIP_GIT_STATUS_AHEAD="${SPACESHIP_GIT_STATUS_AHEAD="⇡"}"
+SPACESHIP_GIT_STATUS_BEHIND="${SPACESHIP_GIT_STATUS_BEHIND="⇣"}"
+SPACESHIP_GIT_STATUS_DIVERGED="${SPACESHIP_GIT_STATUS_DIVERGED="⇕"}"
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# We used to depend on OMZ git library,
+# But it doesn't handle many of the status indicator combinations.
+# Also, It's hard to maintain external dependency.
+# See PR #147 at https://git.io/vQkkB
+# See git help status to know more about status formats
+spaceship_git_status() {
+  [[ $SPACESHIP_GIT_STATUS_SHOW == false ]] && return
+
+  spaceship::is_git || return
+
+  local git_status=""
+  local commits=$(command git rev-list --left-right '@{upstream}...HEAD' 2> /dev/null)
+  local is_ahead=false
+  local is_behind=false
+
+  if [[ $commits ]]; then
+      if $(echo "$commits" | command grep -m1 '>' > /dev/null); then
+        is_ahead=true
+      fi
+
+      if $(echo "$commits" | command grep -m1 '<' > /dev/null); then
+        is_behind=true
+      fi
+  fi
+
+  # Check wheather branch has diverged
+  if [[ "$is_ahead" == true && "$is_behind" == true ]]; then
+    git_status="$SPACESHIP_GIT_STATUS_DIVERGED$git_status"
+  else
+    [[ "$is_ahead" == true ]] && git_status="$SPACESHIP_GIT_STATUS_AHEAD$git_status"
+    [[ "$is_behind" == true ]] && git_status="$SPACESHIP_GIT_STATUS_BEHIND$git_status"
+  fi
+
+  if [[ -n $git_status ]]; then
+    # Status prefixes are colorized
+    spaceship::section \
+      "$SPACESHIP_GIT_STATUS_COLOR" \
+      "$SPACESHIP_GIT_STATUS_PREFIX$git_status$SPACESHIP_GIT_STATUS_SUFFIX"
+  fi
+}
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+
+# Show both git branch and git status:
+#   spaceship_git_branch
+#   spaceship_git_status
+spaceship_simplegit() {
+  [[ $SPACESHIP_GIT_SHOW == false ]] && return
+
+  local git_branch="$(spaceship_git_branch)" git_status="$(spaceship_git_status)"
+
+  [[ -z $git_branch ]] && return
+
+  spaceship::section \
+    'white' \
+    "$SPACESHIP_GIT_PREFIX" \
+    "${git_branch}${git_status}" \
+    "$SPACESHIP_GIT_SUFFIX"
+}
